687d6188f5041ef6714f2cfecbe82f00fedc6db3
Lazureanu George <george.lazureanu@takeofflabs.com>
2021-09-13 13:48:14 +0300

    minor changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def full_title(title = "")
+        if (title.empty?)
+            return "Ruby on Rails Tutorial Sample App"
+        else
+            return title + " | Ruby on Rails Tutorial Sample App"
+        end
+    end
 end
